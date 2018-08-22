e43d7f4939789be6093fba37d6453e38068f9ca1
fanich37 <icima.fan@gmail.com>
2018-08-22 11:16:11 +0300

    refactorig, destroy, resize handler

--- src/smallee.js
+++ src/smallee.js
@@ -50,7 +50,7 @@
     effect: 'slide',
     loop: false,
     resizeTimeout: 1000,
-    responsive: false,
+    responsive: true,
     slidesToScroll: 1,
     slidesToShow: 1,
     swipeable: false,
@@ -81,41 +81,139 @@
     this.settings = mergeSettings(defaultSettings, settings);
     this.settings.transition = `${this.settings.easeFunc} ${this.settings.delay}ms`;
 
-    init.call(this);
+    this.init();
   }
 
-  function prevDefAndStopProp(event) {
+  /**
+   * ----- Public methods -----
+   */
+  Smallee.prototype.init = function() {
+    const fragment = build.call(this, this.settings.effect);
+    this.selector.appendChild(fragment);
+    setData.call(this);
+
+    if (this.settings.arrows) {
+      setArrows.call(this);
+      this.bindGetDirection = getDirection.bind(this);
+      initClickEvents.call(this);
+    }
+
+    if (this.settings.swipeable) {
+      this.bindMouseDown = mouseDown.bind(this);
+      this.bindMouseUp = mouseUp.bind(this);
+      this.bindMouseMove = mouseMove.bind(this);
+      this.bindMouseLeave = mouseLeave.bind(this);
+      this.bindPreventDragStart = preventDragStart.bind(this);
+      initSwipeEvents.call(this);
+    }
+
+    if (this.settings.responsive) {
+      this.bindResizeHandler = resizeHandler.bind(this);
+      initResizeEvent.call(this);
+    }
+
+    /**
+     * It fixes the issue with different selector scrollWidth
+     * value because of scrollBar when there is no exact width value
+     * for the container of the slider.
+     */
+    const timer = setTimeout(() => {
+      clearTimeout(timer);
+      this.refresh();
+    }, 10);
+  };
+
+  Smallee.prototype.changeSlide = function(direction) {
+    const _this = this;
+    let frame;
+    let nextStep;
+
+    switch (direction) {
+      case 'next':
+        if (this.instanceData.wasMovedOn - this.stepRange >= this.scrollLimit) {
+          nextStep = this.instanceData.wasMovedOn - this.stepRange;
+        }
+        if (this.instanceData.wasMovedOn - this.stepRange < this.scrollLimit) {
+          nextStep = this.scrollLimit;
+        }
+        if (this.settings.loop && this.instanceData.wasMovedOn === this.scrollLimit) {
+          nextStep = 0;
+        }
+        break;
+      case 'prev':
+        if (this.instanceData.wasMovedOn + this.stepRange > 0) {
+          nextStep = 0;
+        }
+        if (this.instanceData.wasMovedOn + this.stepRange <= 0) {
+          nextStep = this.instanceData.wasMovedOn + this.stepRange;
+        }
+        if (this.settings.loop && this.instanceData.wasMovedOn === 0) {
+          nextStep = this.scrollLimit;
+        }
+        break;
+      default:
+        return;
+    }
+
+    switch (this.settings.effect) {
+      case 'slide':
+      default:
+        frame = requestAnimationFrame(function() {
+          _this.track.style.transform = `translate3d(${nextStep}px, 0, 0)`;
+          _this.instanceData.wasMovedOn = defineSmalleeWasTranslatedOn.call(_this);
+          restoreTransition.call(_this);
+          setArrowsState.call(_this);
+          cancelAnimationFrame(frame);
+        });
+    }
+  };
+
+  Smallee.prototype.refresh = function() {
+    setData.call(this);
+  };
+
+  Smallee.prototype.onSlideChange = function(func) {
+    func();
+  };
+
+  Smallee.prototype.destroy = function() {
+    const slides = Array.prototype.slice.call(this.track.children);
+
+    this.selector.classList.remove('smallee');
+    this.selector.removeChild(this.inner);
+
+    if (this.settings.arrows) {
+      this.selector.removeChild(this.prev);
+      this.selector.removeChild(this.next);
+      removeClickEvents.call(this);
+    }
+
+    this.settings.swipeable && removeSwipeEvents.call(this);
+    this.settings.responsive && removeResizeEvent.call(this);
+
+    slides.forEach(item => {
+      item.classList.remove('smallee-slide');
+      item.removeAttribute('data-index');
+      item.style.removeProperty('width');
+      this.selector.appendChild(item);
+    });
+  };
+
+  /**
+   * ----- Private methods and utils -----
+   */
+  const prevDefAndStopProp = function(event) {
     event.preventDefault();
     event.stopPropagation();
-  }
+  };
 
-  function mergeSettings(defaultSettings, userSettings) {
+  const mergeSettings = function(defaultSettings, userSettings) {
     for (let key in defaultSettings) {
       if (!userSettings[key]) {
         userSettings[key] = defaultSettings[key];
       }
     }
     return userSettings;
-  }
-
-  const init = function() {
-    const fragment = build.call(this, this.settings.effect);
-    this.sliderCoords = {
-      start: null,
-      wasMovedOn: 0
-    };
-
-    this.selector.appendChild(fragment);
-    setDimensions.call(this);
-    this.stepRange = (this.selector.clientWidth / this.settings.slidesToShow) * this.settings.slidesToScroll;
-    this.scrollLimit = -(
-      (this.numberOfSlides - this.settings.slidesToShow) *
-      (this.stepRange / this.settings.slidesToScroll)
-    );
-
-    this.settings.arrows && setArrows.call(this);
-    this.settings.swipeable && initSwipeEvents.call(this);
-    this.settings.responsive && resizeHandler.call(this);
   };
 
   const build = function(effectType) {
@@ -142,16 +240,33 @@
     return fragment;
   };
 
-  const setDimensions = function() {
+  const setData = function() {
+    this.instanceData = {
+      start: null,
+      wasMovedOn: 0,
+      resized: false
+    };
+
     switch (this.settings.effect) {
       case 'slide':
       default:
-        const sliderWidth = this.selector.clientWidth;
+        const sliderWidth = this.selector.scrollWidth;
+
+        this.stepRange = (sliderWidth / this.settings.slidesToShow) * this.settings.slidesToScroll;
+
+        this.scrollLimit = -(
+          (this.numberOfSlides - this.settings.slidesToShow) *
+          (this.stepRange / this.settings.slidesToScroll)
+        );
+
         this.track.style.width = `${(sliderWidth * this.numberOfSlides) / this.settings.slidesToShow}px`;
+
+        this.track.style.transform = 'translate3d(0, 0, 0)';
+
         this.track.style.transition = this.settings.transition;
+
         this.slides.forEach(item => {
-          item.style.float = 'left';
-          item.style.width = `${100 / this.slides.length}%`;
+          item.style.width = `${sliderWidth / this.settings.slidesToShow}px`;
         });
     }
   };
@@ -174,13 +289,14 @@
 
   const setArrowsState = function() {
     if (!this.settings.arrows || this.settings.loop) return;
-    if (this.sliderCoords.wasMovedOn === this.scrollLimit) {
+
+    if (this.instanceData.wasMovedOn === this.scrollLimit) {
       this.next.classList.add(classes.nextDisabled);
     } else {
       this.next.classList.remove(classes.nextDisabled);
     }
 
-    if (this.sliderCoords.wasMovedOn === 0) {
+    if (this.instanceData.wasMovedOn === 0) {
       this.prev.classList.add(classes.prevDisabled);
     } else {
       this.prev.classList.remove(classes.prevDisabled);
@@ -204,41 +320,40 @@
     if (event.target.closest(`.${classes.next}`)) {
       this.changeSlide('next');
     }
+
     if (event.target.closest(`.${classes.prev}`)) {
       this.changeSlide('prev');
     }
   };
 
-  const initClickEvents = function() {
-    const bindGetDirection = getDirection.bind(this);
-    this.selector.addEventListener('click', bindGetDirection);
-  };
-
   const mouseDown = function(event) {
     this.isDown = true;
-    this.sliderCoords.start = event.clientX;
+    this.instanceData.start = event.clientX;
     clearTransition.call(this);
   };
 
   const mouseUp = function() {
     this.isDown = false;
-    if (event.clientX - this.sliderCoords.start < -this.settings.threshold) {
+
+    if (event.clientX - this.instanceData.start < -this.settings.threshold) {
       this.changeSlide('next');
       return;
     }
-    if (event.clientX - this.sliderCoords.start > this.settings.threshold) {
+
+    if (event.clientX - this.instanceData.start > this.settings.threshold) {
       this.changeSlide('prev');
       return;
     }
+
     restoreTransition.call(this);
-    this.track.style.transform = `translate3d(${this.sliderCoords.wasMovedOn}px, 0, 0)`;
+    this.track.style.transform = `translate3d(${this.instanceData.wasMovedOn}px, 0, 0)`;
   };
 
   const mouseMove = function(event) {
     if (this.isDown && this.settings.effect !== 'fade') {
-      this.track.style.transform = `translate3d(${this.sliderCoords.wasMovedOn +
+      this.track.style.transform = `translate3d(${this.instanceData.wasMovedOn +
         event.clientX -
-        this.sliderCoords.start}px, 0, 0)`;
+        this.instanceData.start}px, 0, 0)`;
     }
   };
 
@@ -246,15 +361,15 @@
     if (this.isDown) {
       this.isDown = false;
       restoreTransition.call(this);
-      if (event.clientX - this.sliderCoords.start < -this.settings.threshold) {
+      if (event.clientX - this.instanceData.start < -this.settings.threshold) {
         this.changeSlide('next');
         return;
       }
-      if (event.clientX - this.sliderCoords.start > this.settings.threshold) {
+      if (event.clientX - this.instanceData.start > this.settings.threshold) {
         this.changeSlide('prev');
         return;
       }
-      this.track.style.transform = `translate3d(${this.sliderCoords.wasMovedOn}px, 0, 0)`;
+      this.track.style.transform = `translate3d(${this.instanceData.wasMovedOn}px, 0, 0)`;
     }
   };
 
@@ -262,88 +377,50 @@
     prevDefAndStopProp(event);
   };
 
-  const initSwipeEvents = function() {
-    const bindMouseDown = mouseDown.bind(this);
-    const bindMouseUp = mouseUp.bind(this);
-    const bindMouseMove = mouseMove.bind(this);
-    const bindMouseLeave = mouseLeave.bind(this);
-    const bindPreventDragStart = preventDragStart.bind(this);
+  /**
+   * Events
+   */
+  const initClickEvents = function() {
+    this.selector.addEventListener('click', this.bindGetDirection);
+  };
 
-    this.selector.addEventListener('mousedown', bindMouseDown);
-    this.selector.addEventListener('mouseup', bindMouseUp);
-    this.selector.addEventListener('mousemove', bindMouseMove);
-    this.selector.addEventListener('mouseleave', bindMouseLeave);
-    this.selector.addEventListener('dragstart', bindPreventDragStart);
+  const removeClickEvents = function() {
+    this.selector.removeEventListener('click', this.bindGetDirection);
+  };
+
+  const initSwipeEvents = function() {
+    this.selector.addEventListener('mousedown', this.bindMouseDown);
+    this.selector.addEventListener('mouseup', this.bindMouseUp);
+    this.selector.addEventListener('mousemove', this.bindMouseMove);
+    this.selector.addEventListener('mouseleave', this.bindMouseLeave);
+    this.selector.addEventListener('dragstart', this.bindPreventDragStart);
+  };
+
+  const removeSwipeEvents = function() {
+    this.selector.removeEventListener('mousedown', this.bindMouseDown);
+    this.selector.removeEventListener('mouseup', this.bindMouseUp);
+    this.selector.removeEventListener('mousemove', this.bindMouseMove);
+    this.selector.removeEventListener('mouseleave', this.bindMouseLeave);
+    this.selector.removeEventListener('dragstart', this.bindPreventDragStart);
+  };
+
+  const initResizeEvent = function() {
+    window.addEventListener('resize', this.bindResizeHandler);
+  };
+
+  const removeResizeEvent = function() {
+    window.removeEventListener('resize', this.bindResizeHandler);
   };
 
   const resizeHandler = function() {
-    const _this = this;
-    this.resized = false;
-
-    window.addEventListener('resize', function() {
-      _this.resized = true;
-      const timer = setTimeout(() => {
-        if (_this.resized) {
-          clearTimeout(timer);
-          setDimensions.call(_this);
-          _this.track.style.transform = 'translate3d{0, 0, 0}';
-          _this.resized = false;
-        }
-      }, _this.settings.resizeTimeout);
-    });
-  };
-
-  Smallee.prototype.changeSlide = function(direction) {
-    const _this = this;
-    let frame;
-    let nextStep;
-
-    switch (direction) {
-      case 'next':
-        if (this.sliderCoords.wasMovedOn - this.stepRange >= this.scrollLimit) {
-          nextStep = this.sliderCoords.wasMovedOn - this.stepRange;
-        }
-        if (this.sliderCoords.wasMovedOn - this.stepRange < this.scrollLimit) {
-          nextStep = this.scrollLimit;
-        }
-        if (this.settings.loop && this.sliderCoords.wasMovedOn === this.scrollLimit) {
-          nextStep = 0;
-        }
-        break;
-      case 'prev':
-        if (this.sliderCoords.wasMovedOn + this.stepRange > 0) {
-          nextStep = 0;
-        }
-        if (this.sliderCoords.wasMovedOn + this.stepRange <= 0) {
-          nextStep = this.sliderCoords.wasMovedOn + this.stepRange;
-        }
-        if (this.settings.loop && this.sliderCoords.wasMovedOn === 0) {
-          nextStep = this.scrollLimit;
-        }
-        break;
-      default:
-        return;
-    }
-
-    switch (this.settings.effect) {
-      case 'slide':
-      default:
-        frame = requestAnimationFrame(function() {
-          _this.track.style.transform = `translate3d(${nextStep}px, 0, 0)`;
-          _this.sliderCoords.wasMovedOn = defineSmalleeWasTranslatedOn.call(_this);
-          restoreTransition.call(_this);
-          setArrowsState.call(_this);
-          cancelAnimationFrame(frame);
-        });
-    }
-  };
-
-  Smallee.prototype.onSlideChange = function(func) {
-    func();
-  };
-
-  Smallee.prototype.destroy = function() {
-    /* TODO */
+    this.instanceData.resized = true;
+    const timer = setTimeout(() => {
+      if (this.instanceData.resized) {
+        clearTimeout(timer);
+        this.refresh();
+        this.instanceData.resized = false;
+      }
+    }, this.settings.resizeTimeout);
   };
 
   return Smallee;
